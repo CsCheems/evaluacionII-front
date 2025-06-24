87702ae2d009b22219479ad0fd0f9d5fcadae481
CsCheems <j.Dev.mx@outlook.com>
2025-06-23 23:51:29 -0600

    se agregaron los demas servicios deacuerdo a los enpoints

--- src/service/categoriasService.js
+++ src/service/categoriasService.js
@@ -5,7 +5,35 @@ const ms_catalogo_categorias = "http://localhost:8001";
 class CategoriasService{
 
     obtenerCategorias(){
-        return axios.get(`${ms_catalogo_categorias}/categorias`);
+        return axios.get(`${ms_catalogo_categorias}/api/categorias`);
+    }
+
+    obtenerCategoriaId(id){
+        return axios.get(`${ms_catalogo_categorias}/api/categorias/${id}`)
+    }
+
+    crearCategoria(categoria){
+        return axios.post(`${ms_catalogo_categorias}/api/categorias`, categoria,{
+            headers:{
+                'Content-Type':'application/json'
+            }
+        });
+    }
+
+    actualizarCategoria(categoria){
+        return axios.put(`${ms_catalogo_categorias}/api/categorias`, categoria,{
+            headers:{
+                'Content-Type':'application/json'
+            }
+        });
+    }
+
+    eliminarCategoria(id){
+        return axios.delete(`${ms_catalogo_categorias}/api/categorias/${id}`);
+    }
+
+    obtenerRequisitos(){
+        return axios.get(`${ms_catalogo_categorias}/api/requisito`);
     }
 
 }
